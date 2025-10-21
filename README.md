b207ac5bd8c59e32ed2cc67175d140ee3c3a217c
Avery <anortonsmith@germantownfriends.org>
2025-10-21 13:27:13 -0400

    update site

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pages
+
+- [./turing/index.html](./turing/index.html)
+- [./sorting/emoji-comparison.html](./sorting/emoji-comparison.html)
+- [./sorting/click-drag.html](./sorting/click-drag.html)
--- transfer.sh
+++ transfer.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 base="../classes/solutions"
 
 mkdir -p turing
@@ -6,3 +8,7 @@ cp -r "$base/tech/turing/index.html" turing
 mkdir -p sorting
 pwd
 cp -r "$base"/cs3/sorting/*.html sorting
+
+echo "# Pages" > README.md
+echo "" >> README.md
+find . -type f -name "*.html" | sed 's/\(.*\)/\- [\1](\1)/' >> README.md
