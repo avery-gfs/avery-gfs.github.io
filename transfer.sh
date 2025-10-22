6a995b595b27e6a392e3da35d5dc22c2989bb4ca
Avery <anortonsmith@germantownfriends.org>
2025-10-21 21:41:36 -0400

    update site

--- transfer.sh
+++ transfer.sh
@@ -2,10 +2,11 @@
 
 base="../classes/solutions"
 
-trash turing sorting
-mkdir -p turing sorting
+trash turing sorting maze
+mkdir -p turing sorting maze
 cp -r "$base"/tech/turing/* turing
 cp -r "$base"/cs3/sorting/*.html sorting
+cp -r "$base"/cs3/sorting/maze.html maze
 
 echo "# Links" > README.md
 echo "" >> README.md
